3eef92864339ddae39eb9ac5fd3a36c88fb4fcb0
xenonzenon <jayson_abad@outlook.com>
2023-10-14 16:32:55 +0800

    ...

--- hello.php
+++ hello.php
@@ -7,8 +7,18 @@
 	</head>
 
 	<body>
+		
 		<h2>Hello World</h2>
-		<a href="/">Home</a>
+		
+		<a href="/">Home</a><br>
+		
+		<?php
+			/*
+				This is a comment block.
+			*/
+			echo "<p>PHP block</p>";
+		?>
+
 	</body>
 
 </html>
--- index.php
+++ index.php
@@ -6,7 +6,9 @@
 
 	<body>
 		<h1>My PHP Code Practice</h1>
+		
 		<a href="hello.php">Hello</a>
+
 		<?php
 			echo "<p><b>This is the php code block</b></p>"
 		?>
